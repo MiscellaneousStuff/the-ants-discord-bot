67659f71b97aade6ae309f67283602d7028c6cfa
Joe Makepeace <joe.makepeace01@gmail.com>
2021-05-20 16:20:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# The Ants Discord Bot
+
+## About
+
+Sends a local notification from a users phone to a discord bot for the ants android game.
+
+## Platforms
+
+Currently supports only Android via logcat at the moment.
---
+++ index.js
@@ -0,0 +1,124 @@
+const execSync = require('child_process').execSync;
+
+const { token, CURRENT_CHANNEL, userId } = require('./config.js');
+
+//const token = "YOUR_TOKEN_GOES_HERE";
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+class Notifications {
+    constructor(timeout = 10) {
+        // Stores moment to moment notifs
+        this.oldNotifs = [];
+        this.curNotifs = [];
+        this.timeout = timeout; // Time between checking for notifications
+        this.timer = setInterval(() => this.loop(), timeout * 1000);
+    }
+    getCur() {
+        this.curNotifs.forEach(notif => this.oldNotifs.push(notif));
+        const toSend = this.curNotifs;
+        this.curNotifs = [];
+        return toSend;
+    }
+    add(notifs) {
+        // Filter null notifs
+        let oldNotifIds = this.oldNotifs.map(notif => (notif.id) ? notif.id : "-1");
+        let curNotifs = [];
+        notifs.forEach(notif => {
+            if (!oldNotifIds.includes(notif.id)) {
+                curNotifs.push(notif);
+            }
+        })
+
+        // Add (and keep) new notifs
+        this.curNotifs = curNotifs;
+    }
+    loop() {
+        console.log('ELLO?')
+        const allNotifs = this.check();
+        this.add(allNotifs);
+        console.log('OOH?', this.oldNotifs.length, this.curNotifs.length);
+        const cur = this.getCur();
+        console.log('DER?', cur.length);
+        cur.forEach(notif => {
+            console.log('SENDING:', notif, CURRENT_CHANNEL);
+            sendMsg(notif.text, CURRENT_CHANNEL);
+        });
+    }
+    check() {
+        const output = execSync('adb shell dumpsys notification', { encoding: 'utf-8' });
+        const lines = output.split("\n");
+
+        let start = false;
+        let notifs = [];
+        let curDetails = {};
+        for (let i=0; i<lines.length; i++) {
+            const line = lines[i].trim();
+            if (line.includes("NotificationRecord")) {
+                if (!start) start = true;
+
+                if (Object.keys(curDetails).length != 0) {
+                    if (curDetails.text != "null") {
+                        notifs.push(curDetails);
+                    }
+                    curDetails = {};
+                }
+
+                const id = line.substring(19, 28);
+                if (line.includes("com.star.union.planetant")) {
+                    curDetails["id"] = id;
+                }
+            } else if (line == "" && start) {
+                // First line after the first 'NotificationRecord' means we've finished
+                if (curDetails.text != "null") {
+                    notifs.push(curDetails);
+                }
+                curDetails = {};
+                break;
+            } else {
+                if (line.includes("tickerText") && start) {
+                    let notifText = line.split("=")[1];
+                    if (notifText == "null") {
+                        curDetails["text"] = notifText; // notifText;
+                    } else {
+                        curDetails["text"] = notifText.substring(5); // Format: tickerText=<br/>content goes here...
+                    }
+                }
+            }
+        }
+        notifs = notifs.filter(notif => Object.keys(notif).length != 0);
+        return notifs;
+    }
+}
+
+let notifs = new Notifications(timeout=2);
+
+const sendMsg = (msg, channelId) => {
+    let fmtMsg = `<@220306503872020480> ${msg}`;
+    //var offTopic = bot.channels.cache.get('448400100591403024'); //.get('448392061415325697');
+    //client.channels.fetch(channelId)
+    //    .then(channel => channel.send(`<@miscguy#4900> ${msg}`));
+    let channel = client.channels.cache.get(channelId);
+    //console.log(channel);
+    //if (channel === undefined) {
+        channel = client.channels.fetch(channelId)
+            .then(channel => {
+                console.log('FUCKING DO SOMETHING?!?!??!??!:', channel);
+                channel.send(fmtMsg)
+            });
+    //}
+    //try { channel.send('hi'); } catch {}
+
+    //const channel = channel.messages.cache.get(channelId);
+    //channel.send('hi');
+
+    //const channel = client.channels.cache.find(ch => ch.name === 'general');
+    //console.log('CHANNEL:', channel)
+    //channel.send('hi');
+}
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.login(token);
